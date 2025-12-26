125bbcf910b895f8ff31a5a3a94b8e53b76c6d57
harshvardhankumararya <90693177+Harshvardhan-2005@users.noreply.github.com>
2025-12-26 12:00:31 +0530

    Initial Commit

---
+++ DeepGuard/DeepGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepGuardApp.swift
+//  DeepGuard
+//
+//  Created by Harshvardhan Kumar on 26/12/25.
+//
+
+import SwiftUI
+
+@main
+struct DeepGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
